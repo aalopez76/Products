606ee8e12ccd82ede660c329aa14f40a315b6628
aAlOpEz <123523752+aalopez76@users.noreply.github.com>
2024-05-17 10:40:37 -0600

    Update products_app.py

--- products_app.py
+++ products_app.py
@@ -6,11 +6,7 @@ import json
 
 key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
-
-
 db = firestore.Client(credentials=creds, project="products-project")
-
-
 dbPRODUCTS = db.collection('products')
 
 st.header('Nuevo registro')
